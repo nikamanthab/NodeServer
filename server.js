7f2a545e05804515a011181a3d7163c3be8e183b
nikamanth appiah <nitinnikamanth@gmail.com>
2018-12-06 01:11:41 +0530

    setup start heroku code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -39,6 +40,6 @@ app.get('/about',(request,response)=>{
 	});
 })
 
-app.listen(3000 ,()=>{
-	console.log("server is started in 3000!")
+app.listen(port ,()=>{
+	console.log(`server is started in ${port}`)
 });
